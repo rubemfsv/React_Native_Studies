7d6000dd172453191d55e49b2e648ed6d93dacbc
Rubem <rfsv@ic.ufal.br>
2020-07-06 15:28:45 -0300

    First commit

---
+++ components/DishdetailComponent.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+
+function RenderDish(props) {
+
+    const dish = props.dish;
+    
+        if (dish != null) {
+            return(
+                <Card
+                featuredTitle={dish.name}
+                image={require('./images/uthappizza.png')}>
+                    <Text style={{margin: 10}}>
+                        {dish.description}
+                    </Text>
+                </Card>
+            );
+        }
+        else {
+            return(<View></View>);
+        }
+}
+
+class Dishdetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    render() {
+        const dishId = this.props.route.params.dishId;
+        return(
+            <RenderDish dish={this.state.dishes[+dishId]} />
+        );
+    }
+}
+export default Dishdetail;
---
+++ components/MainComponent.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { View, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Menu from './MenuComponent';
+import Dishdetail from './DishdetailComponent';
+
+const MenuNavigator = createStackNavigator();
+
+function MenuNavigatorScreen() {
+    return(
+        <MenuNavigator.Navigator
+            initialRouteName='Menu'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    color: "#fff"            
+                }
+            }}
+        >
+            <MenuNavigator.Screen
+                name="Menu"
+                component={Menu}
+            />
+            <MenuNavigator.Screen
+                name="Dishdetail"
+                component={Dishdetail}
+                options={{ headerTitle: "Dish Detail"}}
+            />            
+        </MenuNavigator.Navigator>
+    );
+}
+
+class Main extends Component {
+
+  render() {
+ 
+    return (
+        <NavigationContainer>
+            <MenuNavigatorScreen/>           
+        </NavigationContainer>
+    );
+  }
+}
+  
+export default Main;
