aad4cb29786dd03d2d21497ac8cf101b4d774ca5
Rubem <rfsv@ic.ufal.br>
2020-07-10 14:20:02 -0300

    added alert

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
@@ -26,11 +26,30 @@ class Favorites extends Component {
 
 
         const renderMenuItem = ({ item, index }) => {
+
             const rightButton = [
                 {
                     text: 'Delete',
                     type: 'delete',
-                    onPress: () => this.props.deleteFavorite(item.id)
+                    onPress: () => {
+                        this.props.deleteFavorite(item.id)
+                        // Alert.alert(
+                        //     'Delete Favorite?',
+                        //     'Are you sure you wish to delete the favorite dish ' + item.name + '?',
+                        //     [
+                        //         {
+                        //             text: 'Cancel',
+                        //             onPress: () => console.log(item.name + 'Not Deleted'),
+                        //             style: ' cancel'
+                        //         },
+                        //         {
+                        //             text: 'OK',
+                        //             onPress: () => this.props.deleteFavorite(item.id)
+                        //         }
+                        //     ],
+                        //     { cancelable: false }
+                        // );
+                    }
                 }
             ];
 
