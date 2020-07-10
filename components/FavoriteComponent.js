52d8fff1afd49d6c8239016be3c1b76cf90bf6b3
Rubem <rfsv@ic.ufal.br>
2020-07-10 14:09:08 -0300

    delete favorites

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -4,57 +4,73 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
-      dishes: state.dishes,
-      favorites: state.favorites
+        dishes: state.dishes,
+        favorites: state.favorites
     }
-  }
+}
+
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+});
 
 class Favorites extends Component {
 
     render() {
 
         const { navigate } = this.props.navigation;
-        
-        const renderMenuItem = ({item, index}) => {
-    
+
+
+        const renderMenuItem = ({ item, index }) => {
+            const rightButton = [
+                {
+                    text: 'Delete',
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ];
+
             return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
-                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        hideChevron={true}
+                        onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                        leftAvatar={{ source: { uri: baseUrl + item.image } }}
                     />
+                </Swipeout>
             );
         };
 
         if (this.props.dishes.isLoading) {
-            return(
+            return (
                 <Loading />
             );
         }
         else if (this.props.dishes.errMess) {
-            return(
-                <View>            
+            return (
+                <View>
                     <Text>{this.props.dishes.errMess}</Text>
-                </View>            
+                </View>
             );
         }
         else {
             return (
-                <FlatList 
+                <FlatList
                     data={this.props.dishes.dishes.filter(dish => this.props.favorites.some(el => el === dish.id))}
                     renderItem={renderMenuItem}
                     keyExtractor={item => item.id.toString()}
-                    />
+                />
             );
         }
     }
 }
 
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -155,6 +155,11 @@ export const addFavorite = (dishId) => ({
     payload: dishId
 });
 
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+});
+
 export const postComment = (dishId, rating, author, comment) => (dispatch) => {
     var newComment = {
         dishId: dishId,
