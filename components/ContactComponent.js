cdde835c4373523bfb35dae86d11a19205792d3e
Rubem <rfsv@ic.ufal.br>
2020-07-13 21:22:19 -0300

    Add Social Sharing

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ScrollView, Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
-function ContactUs() {
-    return (
-        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
-            <Card
-                title="Contact Information"
-            >
-                <Text style={{ marginBottom: 10 }}>121, Clear Water Bay Road</Text>
-                <Text style={{ marginBottom: 10 }}>Clear Water Bay, Kowloon</Text>
-                <Text style={{ marginBottom: 10 }}>HONG KONG</Text>
-                <Text style={{ marginBottom: 10 }}>Tel: +852 1234 5678</Text>
-                <Text style={{ marginBottom: 10 }}>Fax: +852 8765 4321</Text>
-                <Text style={{ marginBottom: 10 }}>Email:confusion@food.net</Text>
+class ContactUs extends Component {
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        })
+    }
 
-            </Card>
-        </Animatable.View>
-    );
+    render() {
+        return (
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                <Card
+                    title="Contact Information"
+                >
+                    <Text style={{ marginBottom: 10 }}>121, Clear Water Bay Road</Text>
+                    <Text style={{ marginBottom: 10 }}>Clear Water Bay, Kowloon</Text>
+                    <Text style={{ marginBottom: 10 }}>HONG KONG</Text>
+                    <Text style={{ marginBottom: 10 }}>Tel: +852 1234 5678</Text>
+                    <Text style={{ marginBottom: 10 }}>Fax: +852 8765 4321</Text>
+                    <Text style={{ marginBottom: 10 }}>Email:confusion@food.net</Text>
+                    <Button
+                        title="Send Email"
+                        buttonStyle={{ backgroundColor: "#512DA8" }}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                        onPress={this.sendMail}
+                    />
+                </Card>
+            </Animatable.View>
+        );
+    }
 }
 
 export default ContactUs;
--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -68,6 +68,16 @@ function RenderDish(props) {
         }
     })
 
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        }, {
+            dialogTitle: 'Share ' + title
+        })
+    }
+
     if (dish != null) {
         return (
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
@@ -95,6 +105,14 @@ function RenderDish(props) {
                             color='#512DA8'
                             onPressModal={() => props.showModal ? console.log('Already opened') : props.onPressModal()}
                         />
+                        <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            style={styles.cardItem}
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} />
                     </View>
                 </Card>
             </Animatable.View>
